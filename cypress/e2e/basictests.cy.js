6de41e6e4f47fab2ced5504246874dd1503d3cc9
Dranniechek <anna.surnova@gmail.com>
2023-05-20 12:29:48 +0200

    steps without checks

--- cypress/e2e/basictests.cy.js
+++ cypress/e2e/basictests.cy.js
@@ -10,4 +10,29 @@ describe('Basic functionality', () => {
         cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
         
     })
+    
+    it('Purchase of one item', () => {
+
+        cy.visit('https://www.demoblaze.com')
+
+        cy.get(':nth-child(1) > .card > :nth-child(1) > .card-img-top').click()
+
+        cy.get('.col-sm-12 > .btn').click()
+
+        cy.get('#cartur').click()
+        cy.get('.col-lg-1 > .btn').click()
+
+        cy.get('#name').type('Testuser')
+        cy.get('#country').type('TestCountry')
+        cy.get('#city').type('Testcity')
+        cy.get('#card').type('1234432198766789')
+        cy.get('#month').type('May')
+        cy.get('#year').type('2023')
+
+        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+
+        cy.get('.confirm').click()
+
+    })
+
 })
