e5841997a5e61f9ec4f034c4412ebe27215372ae
Dranniechek <anna.surnova@gmail.com>
2023-06-10 20:07:25 +0200

    Cypress Best Practices p.1

--- cypress/e2e/basictests.cy.js
+++ cypress/e2e/basictests.cy.js
@@ -1,50 +1,51 @@
 describe('Basic functionality', () => {
+
+    beforeEach(() => {
+        // open website
+        cy.visit('/')
+    })
+
+    //Cypress._.times(10, () => {
     it('Create new user', () => {
         
-        const timestamp = Date.now();
-        //const randomNumber = Math.floor(Math.random() * timestamp);
-
-        // open website
-        cy.visit('https://www.demoblaze.com')
-
+        const userName = ('Testuser' + Date.now())
+        
         // Click on "Sign up"
         cy.get('#signin2').click()
         
         // Fill in username and password
-        //cy.get('#sign-username').type('Testuser' + randomNumber.toString())
-        cy.wait(1000)
-        cy.get('#sign-username').type('Testuser' + timestamp.toString())
-        cy.get('#sign-password').type('Passwordtest')
+        
+        cy.get('#signInModal').should('be.visible')
+        cy.get('#sign-username').invoke('val', userName)
+        cy.get('#sign-password').invoke('val', 'Passwordtest')
 
         // check that text "Sign up successful." appeared 
         cy.window().then((win) => {
             cy.stub(win, 'alert').as('winAlert')
             //  Click "Sign up" button
-            cy.xpath("//button[@type='button' and @onclick='register()']").click()
+            cy.get('#signInModal button.btn-primary').click()
             cy.get('@winAlert').should('be.calledWith', 'Sign up successful.')
           })
         
     })
+//})
     
     it('Purchase of one item', () => {
 
-        // open website
-        cy.visit('https://www.demoblaze.com')
-
         // Open first product
-        cy.xpath('//a[@href="prod.html?idp_=1" and @class="hrefch"]').click()
-
+        cy.contains('.hrefch', 'Samsung galaxy s6').click()
+        
         //Click add to cart
-        cy.xpath('//a[contains(@class, "btn-success")]').click()
+        cy.get('#tbodyid a.btn-success').click()
 
         // Open Cart
         cy.get('#cartur').click()
 
         // check if there is an item in the cart
-        cy.xpath('//*[contains(@onclick, "deleteItem")]').should('be.visible')
+        cy.contains('.table', 'Delete').should('be.visible')
 
         // Click "Place Order"
-        cy.xpath('//*[contains(@class, "btn-success")]').click()
+        cy.get('#page-wrapper button.btn-success').click()
 
         // Fill in all fields on the page
         cy.get('#name').type('Testuser')
@@ -55,16 +56,15 @@ describe('Basic functionality', () => {
         cy.get('#year').type('2023')
 
         // Click "Purchase" button
-        cy.xpath('//*[contains(@onclick, "purchaseOrder")]').click()
+        cy.get('#orderModal button.btn-primary').click()
 
         // Checking for a successful purchase icon
-        cy.xpath('//*[contains(@class, "sa-placeholder")]').should('be.visible')
+        cy.contains('.sweet-alert', 'Thank you for your purchase!').should('be.visible')
 
         // Click "Ok" button
         cy.wait(1000)
         cy.get('button.confirm').click()
        
-
         // check if main page open after purchase
         cy.url().should('include', 'index.html')
 
